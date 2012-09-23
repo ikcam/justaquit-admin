6d1e3e58b22ffda8a7339fd6a62997e0838cac60
Irving Kcam <xploit29@gmail.com>
2012-09-23 12:09:26 -0500

    Add function has_domains()

--- jadmin_functions.php
+++ jadmin_functions.php
@@ -8,20 +8,42 @@ Functions
 *	 get_client( $ID )
 *
 * Args: 
-* 	ID: The client ID
+* 	ID: Client ID
 *
 * Return:
 * 	Array with client information.
 */
 function get_client( $ID ){
 	global $wpdb;
-
 	$table  = $wpdb->prefix.'clients';
-	$query  = "SELECT * FROM $table WHERE ID = %d";
+
+	$query  = "SELECT * FROM $table WHERE ID = %d;";
 	$client = $wpdb->get_row( $wpdb->prepare($query, $ID) );
 
 	return $client;
 }
 
+/*
+* Name:
+* 	has_domains( $ID )
+*
+* Args:
+* 	ID: Client ID
+* 
+* Return:
+* 	True if has domains or False if doesn't.
+*/
+function has_domains( $ID ){
+	global $wpdb;
+	$table = $wpdb->prefix.'domains';
+
+	$query = "SELECT COUNT(*) FROM $table WHERE client_id = %d;";
+	$result = $wpdb->get_var( $wpdb->prepare($query, $ID) );
+
+	if(  $result > 0 )
+		return TRUE;
+	else
+		return FALSE;
+}
 
 ?>
