1708841f26541e483ae356b47e5dda16a38f2ddc
Irving Kcam <xploit29@gmail.com>
2012-09-26 09:48:19 -0500

    Database delete function

--- pages/database_delete.php
+++ pages/database_delete.php
@@ -4,18 +4,18 @@ function jadmin_page_database_delete(){
 
 	$database = get_database( $ID );
 
-	if( $database == NULL ) :
-?>
-<div id="message" class="error">
-	<p>The database that you are looking for doesn't exists.</p>
-</div>
-<?php
-	else:
-		$delete = new Database( 'delete_database', $ID );
-		$delete->delete_database( $ID );
+	if( is_array($database) ) :
+		$delete = new Database( 'delete_database', $database->ID );
+		$delete->delete_database( $database->ID );
 ?>
 <div id="message" class="updated">
 	<p>The detabase has been deleted succesfuly.</p>
+</div>
+<?php
+	else:
+?>
+<div id="message" class="error">
+	<p>The database that you are looking for doesn't exists.</p>
 </div>
 <?php
 	endif;
