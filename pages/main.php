4c908f983c9bcdc7828a336eadaa7c90f093e29e
Irving Kcam <xploit29@gmail.com>
2012-09-27 23:20:59 -0500

    Main page

--- pages/main.php
+++ pages/main.php
@@ -1,5 +1,32 @@
 <?php
 function jadmin_page_main(){
-	echo 'Main';
+?>
+<div class="wrap">
+	<div id="icon-index" class="icon32"><br></div>
+	<h2>JustAquit Admin Center</h2>
+
+	<table class="form-table">
+		<h3>Comming soon:</h3>
+		<ul style="padding-left:20px;list-style:disc;">
+			<li>Use <code>wp_nonce_field</code> in forms.</li>
+			<li>Add test mode</li>
+			<li>Use <a href="//name.com" target="_black">Name.com</a> API for domain management.
+			<li>More Linode functions</li>
+			<li>Add/manage clusters</li>
+		</ul>
+		<p>&nbsp;</p>
+		<p>
+			Forked in <a href="https://github.com/ikcam/justaquit-admin" target="_blank">GitHub</a> |
+			<a href="https://github.com/ikcam/justaquit-admin/issues" target="_blank">Report an issue</a>
+		</p>
+		<p>
+			Creator: <a href="//ikcam.com" target="_blank">Irving Kcam</a> |
+			Email: <a href="mailto:me@ikcam">me@ikcam.com</a> |
+			License: GPL2
+		</p>
+
+	</table>
+</div>
+<?php
 }
 ?>
