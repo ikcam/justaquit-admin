93d9fa39d0a0c7249a5be30afa2f62586e0af1fd
Irving Kcam <xploit29@gmail.com>
2012-11-08 09:59:47 -0500

    Delete Page

---
+++ pages/project_delete.php
@@ -0,0 +1,22 @@
+<?php
+function jadmin_page_project_delete(){
+	$ID = $_GET['ID'];
+
+	$project = get_project( $ID );
+
+	if( is_object($project) ) :
+?>
+		<div id="message" class="error">
+			<p>The project that you are looking for doesn't exists</p>
+		</div>
+<?php
+	else:
+		Client::delete_project($ID);
+?>
+		<div id="message" class="updated">
+			<p>The project has been deleted succesfuly.</p>
+		</div>
+<?php
+	endif;
+}
+?>
