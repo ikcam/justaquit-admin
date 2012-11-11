e9324c92e6deddb6e6d07da2793de89cff79539a
Irving Kcam <xploit29@gmail.com>
2012-11-11 02:59:51 -0500

    Full bitbucket support (BitBucket shit JSON)

--- includes/shortcode-git.php
+++ includes/shortcode-git.php
@@ -8,16 +8,22 @@ Class shortcode_git {
 		if( isset($_POST['payload']) ):
 			$push = json_decode(stripslashes($_POST['payload']), true);
 			$url = $push['repository']['url'];
-			if( $url = '' )
-				$url = $push['repository']['website'];
+			if( $url = '' ){
+				$absolute_url = $push['repository']['absolute_url'];
+				$absolute_url = substr( $absolute_url, 1, strlen($absolute_url) );
+				$website = $push['repository']['website'];
+				$url = $website.$absolute_url;
+			}
+			
+			if( $url != '' ){
+				$projects = get_projects_by_url($url);
 
-			$projects = get_projects_by_url($url);
-
-			if( $projects ):
-				foreach( $projects as $project ):
-					update_git($project->ID);
-				endforeach;
-			endif;
+				if( $projects ):
+					foreach( $projects as $project ):
+						update_git($project->ID);
+					endforeach;
+				endif;
+			}
 		endif;
 	}
 }
