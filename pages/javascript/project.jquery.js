419ff580485ddfd6c966c4adcf581161973ee9c4
Irving Kcam <xploit29@gmail.com>
2012-11-10 23:01:32 -0500

    Fix when selected "none"

--- pages/javascript/project.jquery.js
+++ pages/javascript/project.jquery.js
@@ -1,8 +1,12 @@
 jQuery(document).ready(function($){
 	var base_name = $('#server_folder_base').attr('value');
 	$('#domain_id').change(function(){
-		var domain_name = $('option:selected', this).attr('rel');
-		var full_name = base_name + domain_name + '/';
-		$('#location_base').attr('value',full_name);
+		if( $('option:selected').attr('value') == 0 ){
+			$('#location_base').attr('value',base_name);
+		} else {
+			var domain_name = $('option:selected', this).attr('rel');
+			var full_name = base_name + domain_name + '/';
+			$('#location_base').attr('value',full_name);			
+		}
 	});
 });
